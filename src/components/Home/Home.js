9892ea2d9bf219183ef2905fc7611bc5c4ca8aac
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-30 02:38:55 +0600

    add home pages

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { CurrencyBangladeshiIcon } from '@heroicons/react/solid'
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='flex flex-col h-[80vh] justify-center items-center'>
+            <h1 className='font-mono text-xl md:text-6xl'>Welcome to CRYPTO CAFE <span className='text-xl md:text-6xl'>☕</span></h1>
+            <Link to={'/coins'} className='bg-cyan-600 text-white py-2 px-3 rounded-full mt-3' >Explore Coins</Link>
         </div>
     );
 };
