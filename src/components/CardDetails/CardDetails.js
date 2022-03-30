5db7ef295fec2d7a1a7b813574955433db135cc6
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-30 15:21:59 +0600

    add card details

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -13,10 +13,11 @@ const CardDetails = () => {
             .then(data => setCoin(data))
     }, [id])
     return (
-        <div>
-            <div>
-                <div>
+        <div className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
+            <div className='h-full grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 md:justify-items-around content-center'>
+                <div className='order-2 md:order-1'>
                     <h1 className='text-3xl text-gray-600'>General Info</h1>
+                    <hr />
                     <p>Coin Name: {name}</p>
                     <p>Market Cap Rank: {market_cap_rank}</p>
                     <p>Origin: {country_origin ? country_origin : 'Not Available'}</p>
@@ -24,16 +25,16 @@ const CardDetails = () => {
                     <p>Hashing Algorithm: {hashing_algorithm}</p>
                     <p>Genesis Data: {genesis_date}</p>
                     <p>Last Updated: {last_updated}</p>
-                </div>
-                <div>
-                    <h1 className='text-3xl text-gray-600'>Scores</h1>
+
+                    <h1 className='text-3xl text-gray-600 mt-4'>Scores</h1>
+                    <hr />
                     <p>Community Score: {community_score}</p>
                     <p>Developer Score: {developer_score}</p>
                     <p>Liquidity Score: {liquidity_score}</p>
                     <p>Public Interest Score: {public_interest_score}</p>
                 </div>
             </div>
-            <div>
+            <div className='flex order-1 md:order-2 justify-center items-center'>
                 <img src={image?.large} alt="" />
             </div>
         </div>
